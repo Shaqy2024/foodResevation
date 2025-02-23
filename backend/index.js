bf59debab7fb827fa2257f89f41022284ea1e4bf
Shaqy2024 <sakib950006@gmail.com>
2025-02-23 15:37:27 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors(
     {
-        origin: "http://localhost:3000",
+        origin: "https://food-resevation-wziq.vercel.app",
         credentials: true
 
     }
